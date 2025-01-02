2cea804a5f07df4050ad52aadaeec92ef7123608
Cocomomilkshake <76200257+Cocomomilkshake@users.noreply.github.com>
2025-01-02 19:53:34 +0800

    Delete introduction.py

--- introduction.py
+++
@@ -1,6 +0,0 @@
-name= input( "whats your name")
-age= input( "whats ur age lil bro?")
-favourite_food = input( "whats ur favourite food?")
-print(f"\nyour name is {name} ,your age is {age} and your favourite food is {favourite_food}") 
-import os
-os.system("pause")
